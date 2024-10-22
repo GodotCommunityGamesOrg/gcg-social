b646ff3157ee23c696bdbe2ada43311e3f6f305a
The Bard Who Codes <shaynecodes@gmail.com>
2024-10-21 22:08:34 -0400

    Added camera follow to player

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -3,9 +3,28 @@ class_name Player
 
 const SPEED = 300.0
 
+func _ready() -> void:
+	give_control_to_the_proper_client()
+	if this_is_my_player():
+		follow_it_with_a_camera()
+
 func _physics_process(delta):
-	var peer_id = name.to_int()
-	set_multiplayer_authority(peer_id)
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * SPEED
 	move_and_slide()
+
+func get_peer_id() -> int:
+	return name.to_int()
+
+func this_is_my_player() -> bool:
+	var the_clients_peer_id = multiplayer.get_unique_id()
+	var this_players_peer_id = get_peer_id()
+	return the_clients_peer_id == this_players_peer_id
+
+func give_control_to_the_proper_client() -> void:
+	var peer_id = get_peer_id()
+	set_multiplayer_authority(peer_id) # Setting the multiplayer authority tells the engine which peer is in charge of this node
+
+func follow_it_with_a_camera() -> void:
+	var camera = Camera2D.new()
+	add_child(camera)
